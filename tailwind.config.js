2fc76768d59061155adcfd80e22c26dbb520061e
ArKc1009 <arkc1009@gmail.com>
2022-07-28 16:15:58 +0900

    chore: twinmacro가 tailwind의 colors를 가져오기 못하는 이슈 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: { colors },
   },
   plugins: [],
-}
+};
