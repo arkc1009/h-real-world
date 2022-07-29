b601e8f625a58d067a7da0a2840e214044fcd67a
ArKc1009 <arkc1009@gmail.com>
2022-07-29 18:03:56 +0900

    feat: Login Form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from 'next';
 import tw from 'twin.macro';
+import Page from '../components/common/Page';
+import PageContent from '../components/common/PageContent';
+import LoginForm from '../components/Form/LoginForm';
+
+// background-color: #449d44;
+// border-color: #419641;
 
 const Login: NextPage = () => {
   return (
-    <div css={tw`flex min-h-screen flex-col items-center justify-center py-2`}>
-      Login Page!
-    </div>
+    <Page>
+      <PageContent>
+        <h1 css={tw`text-4xl mb-3`}>Sign in</h1>
+        <span css={tw`text-[#5CB85C] text-base`}>Need an account?</span>
+
+        <LoginForm />
+      </PageContent>
+    </Page>
   );
 };
 
