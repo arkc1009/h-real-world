8650bc1ee008cef19cbe47f4c96562f2a5dac384
ArKc1009 <arkc1009@gmail.com>
2022-08-19 02:30:33 +0900

    fix: Banner 컴포넌트 로고 배너로 변경

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,22 +1,30 @@
+import { motion } from 'framer-motion';
 import { HTMLAttributes } from 'react';
-import tw, { css } from 'twin.macro';
+import tw from 'twin.macro';
 
 interface BannerProps extends HTMLAttributes<HTMLDivElement> {
   bgColor?: string;
 }
 
-const Banner: React.FC<BannerProps> = ({ children, bgColor = '#5CB85C', ...rest }) => {
+const Banner: React.FC<BannerProps> = ({ ...rest }) => {
   return (
     <div
       css={[
-        tw`w-full h-44 shadow-inner`,
-        css`
-          background-color: ${bgColor};
-        `,
+        tw`w-full h-60 flex justify-center items-center shadow-inner`,
+        tw`bg-slate-100`,
+        tw`select-none`,
       ]}
       {...rest}
     >
-      {children}
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        whileHover={{ scale: 1.1 }}
+        src='images/logo.png'
+        alt=''
+        css={tw`w-96 overflow-hidden z-0`}
+      />
     </div>
   );
 };
