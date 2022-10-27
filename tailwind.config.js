8eef09d171ace032498a8256d0273aca75911eab
한동진 <arkc1009@gmail.com>
2022-10-27 18:16:26 +0900

    chore(tailwind): 기존 설정 extends로 변경, fontsize xxs 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const colors = require('tailwindcss/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: { ...colors, onlyOne1: '#CCF2FF', onlyOne2: '#B9CAFF', onlyOne3: '#FFB2C5' },
+    extend: {
+      colors: {
+        onlyOne1: '#CCF2FF',
+        onlyOne2: '#B9CAFF',
+        onlyOne3: '#FFB2C5',
+      },
+      fontSize: {
+        xxs: [
+          '0.625rem',
+          {
+            lineHeight: '0.75rem',
+            fontWeight: '400',
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
